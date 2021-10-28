6f3338a4f4702241166de96d4e0ba1691f3d978d
SergiITB <sergi.santos.7e4@itb.cat>
2021-10-28 16:25:04 +0200

    .

---
+++ src/main/java/cat/itb/sldproject/apirest/sldproject/SldProjectApplication.java
@@ -0,0 +1,13 @@
+package cat.itb.sldproject.apirest.sldproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SldProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SldProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/itb/sldproject/apirest/sldproject/SldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itb.sldproject.apirest.sldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SldProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
